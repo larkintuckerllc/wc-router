7a66be1e20b3fdf7d12c1870dc7ffccb42ee88b6
John Tucker <jtucker@appirio.com>
2019-08-20 15:53:49 -0400

    webpack

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,10 @@
-((): void => {
-  class HelloWorld extends HTMLElement {
-    constructor() {
-      super();
-      const shadow = this.attachShadow({ mode: 'closed' });
-      const rootEl = document.createElement('div');
-      rootEl.textContent = 'Hello World';
-      shadow.appendChild(rootEl);
-    }
+class HelloWorld extends HTMLElement {
+  constructor() {
+    super();
+    const shadow = this.attachShadow({ mode: 'closed' });
+    const rootEl = document.createElement('div');
+    rootEl.textContent = 'Hello World';
+    shadow.appendChild(rootEl);
   }
-  window.customElements.define('hello-world', HelloWorld);
-})();
+}
+window.customElements.define('hello-world', HelloWorld);
