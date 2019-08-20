107013d96b0bbc45bc8117445ca7f4c5295ebffa
John Tucker <jtucker@appirio.com>
2019-08-20 11:26:00 -0400

    first commit

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+((): void => {
+  class HelloWorld extends HTMLElement {
+    constructor() {
+      super();
+      const shadow = this.attachShadow({ mode: 'closed' });
+      const rootEl = document.createElement('div');
+      rootEl.textContent = 'Hello World';
+      shadow.appendChild(rootEl);
+    }
+  }
+  window.customElements.define('hello-world', HelloWorld);
+})();
