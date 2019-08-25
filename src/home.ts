fe7ff4da79815aa8db1c3216c7bc0f1218b8a496
John Tucker <jtucker@appirio.com>
2019-08-25 07:21:45 -0400

    Home

--- src/home.ts
+++ src/home.ts
@@ -4,8 +4,14 @@ const handleAClick = (): void => {
   Router.go('/a');
 };
 
+const handleBClick = (): void => {
+  Router.go('/b');
+};
+
 class Home extends HTMLElement {
   private rootAEl: HTMLDivElement;
+
+  private rootBEl: HTMLDivElement;
 
   constructor() {
     super();
@@ -18,15 +24,23 @@ class Home extends HTMLElement {
     this.rootAEl.setAttribute('style', 'text-decoration: underline; cursor: pointer;');
     rootEl.appendChild(this.rootAEl);
 
+    // B
+    this.rootBEl = document.createElement('div');
+    this.rootBEl.textContent = 'B';
+    this.rootBEl.setAttribute('style', 'text-decoration: underline; cursor: pointer;');
+    rootEl.appendChild(this.rootBEl);
+
     shadow.appendChild(rootEl);
   }
 
   public connectedCallback(): void {
     this.rootAEl.addEventListener('click', handleAClick);
+    this.rootBEl.addEventListener('click', handleBClick);
   }
 
   public disconnectedCallback(): void {
     this.rootAEl.removeEventListener('click', handleAClick);
+    this.rootBEl.removeEventListener('click', handleBClick);
   }
 }
 
